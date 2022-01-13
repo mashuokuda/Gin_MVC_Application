2551517fc865177869b0dd02b30bae40a37276f2
saffron <y.saffrontea@gmail.com>
2022-01-13 12:00:33 +0900

    login処理をかいた

--- router/router.go
+++ router/router.go
@@ -2,15 +2,22 @@ package router
 
 import (
 	"Gin_MVC/controller"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() *gin.Engine {
 	router := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("graduate", store))
 	router.LoadHTMLGlob("view/*.html")
 	router.Static("resource", "./resource")
 
 	router.GET("/", controller.IndexDisplayAction)
 	router.GET("/discuss", controller.Display)
+	router.GET("/login", controller.DisplayLoginFrom)
+	router.POST("/doAuth", controller.DoAuth)
+
 	return router
 }
