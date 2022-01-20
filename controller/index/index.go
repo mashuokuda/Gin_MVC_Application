a1b6713fc7c05accf53c25c96d44511a68c3f8d7
M0c48n <d41ki103@gmail.com>
2022-01-20 12:26:45 +0900

    ディレクトリ構造変更、コメント追加

---
+++ controller/index/index.go
@@ -0,0 +1,30 @@
+/*
+	index.go
+	indexページ処理
+*/
+package index
+
+import (
+	"Gin_MVC/controller/login"
+	"github.com/gin-gonic/gin"
+)
+
+func IndexDisplayAction(c *gin.Context) {
+	errorMsg := ""
+	usr, loginState, err := login.GetLoginUser(c)
+	if err != nil {
+		errorMsg = err.Error()
+	}
+
+	c.HTML(200, "index.html", gin.H{
+		//ユーザ名
+		"username": usr.GetUserName(),
+		//自己紹介
+		"userprofile": usr.Profile,
+		//アイコン
+		"img": usr.Image.GetImage(),
+		//ログイン状態
+		"loginState": loginState,
+		"errorMsg":   errorMsg,
+	})
+}
