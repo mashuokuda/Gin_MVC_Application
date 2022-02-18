224221261d2508c92fb497b47da04645a30bddfb
mashuokuda <o9dm4u@yahoo.co.jp>
2022-02-18 16:16:57 +0900

    index画面のモックアップを作成

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,1 +1,12 @@
-console.log("Hello Gin");
+// trタグにリンクを作成
+$(() => {
+  $('tr[data-href]').addClass('clickable').click(function() {
+    window.location = $(this).attr('data-href');
+  }).find('a').hover(function() {
+    $(this).parents('tr').unbind('click');
+  }, function() {
+    $(this).parents('tr').click(function() {
+      window.location = $(this).attr('data-href');
+    });
+  });
+});
